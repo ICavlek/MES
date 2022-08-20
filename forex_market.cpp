61fe2bbcc24a46e69dd9c2e7e8171717a94fb338
Ivan <ivan.cavlek1403@gmail.com>
2022-08-20 17:10:06 +0200

    Additional changes for demo - Test SUCCESS

--- forex_market.cpp
+++ forex_market.cpp
@@ -4,38 +4,49 @@
 
 #include "DataBaseHandler.hpp"
 
-std::vector<PriceData> get_ask_data_from_database(std::string currency_pair, std::string start_date, std::string start_time, std::string end_date, std::string end_time) {
-	std::cout << "Loading ASK data..." << std::endl;
-	DataBaseFileHandler data_base(
-		currency_pair, "ASK", start_date, start_time, end_date, end_time
-	);
-	return data_base.get_data_vector();
-}
-
-std::vector<PriceData> get_bid_data_from_database(std::string currency_pair, std::string start_date, std::string start_time, std::string end_date, std::string end_time) {
-	std::cout << "Loading BID data..." << std::endl;
-	DataBaseFileHandler data_base(
-		currency_pair, "BID", start_date, start_time, end_date, end_time
-	);
-	return data_base.get_data_vector();
-}
-
-void simulate_expert_advisor(ExpertAdvisor& expert_advisor, std::vector<PriceData> ask_data_vector, std::vector<PriceData> bid_data_vector) {
-	for (size_t i = 0; i < ask_data_vector.size(); i++) {
-		std::string date_time = ask_data_vector[i].date_time;
-		double price_ask = ask_data_vector[i].price;
-		double price_bid = bid_data_vector[i].price;
-
-		std::cout << date_time + " ";
-		expert_advisor.api_handler.update_orders(date_time, price_ask, price_bid);
-		expert_advisor.handle_state_machine(price_ask, price_bid);
+class ForexMarket {
+public:
+	ForexMarket(std::string currency_pair, std::string start_date, std::string start_time, std::string end_date, std::string end_time) :
+		currency_pair(currency_pair),
+		start_date(start_date),
+		start_time(start_time),
+		end_date(end_date),
+		end_time(end_time) {
+		ask_data = get_data_from_database("ASK");
+		bid_data = get_data_from_database("BID");
 	}
-}
 
-void forex_market(ExpertAdvisor& expert_advisor, std::string currency_pair, std::string start_date, std::string start_time, std::string end_date, std::string end_time) {
-	auto ask_data_vector = get_ask_data_from_database(currency_pair, start_date, start_time, end_date, end_time);
-	auto bid_data_vector = get_bid_data_from_database(currency_pair, start_date, start_time, end_date, end_time);
+	void run_strategy(ExpertAdvisor& expert_advisor) {
+		expert_advisor.initialize(ask_data[0].price);
+		simulate_expert_advisor(expert_advisor);
+	}
 
-	expert_advisor.initialize(ask_data_vector[0].price);
-	simulate_expert_advisor(expert_advisor, ask_data_vector, bid_data_vector);
-}
+private:
+	std::string currency_pair;
+	std::string start_date;
+	std::string start_time;
+	std::string end_date;
+	std::string end_time;
+	std::vector<PriceData> ask_data;
+	std::vector<PriceData> bid_data;
+
+	std::vector<PriceData> get_data_from_database(std::string type) {
+		std::cout << "Loading " + type + " data..." << std::endl;
+		DataBaseFileHandler data_base(
+			currency_pair, type, start_date, start_time, end_date, end_time
+		);
+		return data_base.get_data_vector();
+	}
+
+	void simulate_expert_advisor(ExpertAdvisor& expert_advisor) {
+		for (size_t i = 0; i < ask_data.size(); i++) {
+			std::string date_time = ask_data[i].date_time;
+			double price_ask = ask_data[i].price;
+			double price_bid = bid_data[i].price;
+
+			std::cout << date_time + " ";
+			expert_advisor.api_handler.update_orders(date_time, price_ask, price_bid);
+			expert_advisor.handle_state_machine(price_ask, price_bid);
+		}
+	}
+};
--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@ int main() {
 	std::string start_time = "00:00:00.000";
 	std::string end_date = "02.02.2022";
 	std::string end_time = "23:59:00.000";
+	ForexMarket forex_market(currency_pair, start_date, start_time, end_date, end_time);
 
 	try {
-		forex_market(expert_advisor, currency_pair, start_date, start_time, end_date, end_time);
+		forex_market.run_strategy(expert_advisor);
 	}
 	catch (MOHBaseException& e) {
 		std::cout << e.what() << std::endl;
